8afcecf32509c20eee1401677771f7638f0d3124
editaukago <editaukago.student@gmail.com>
2026-05-22 01:36:09 +0700

    chore: konfigurasi provider dan route console

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
-    $this->comment('Build clean systems.');
+    $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
